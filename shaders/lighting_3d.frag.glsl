fb47fc9c13eb994895253e25688cce3419135e16
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-08-15 10:31:36 +0300

    Make phong shading with directional lights

--- shaders/lighting_3d.frag.glsl
+++ shaders/lighting_3d.frag.glsl
@@ -1,15 +1,48 @@
 #version 330 core 
 
-out vec4 fragColor;
+struct Material {
+  sampler2D diffuse;
+  sampler2D specular;
+  float shininess;
+};
 
-uniform vec4 color;
-uniform vec3 lightColor = vec3(1.0);
+struct DirectionalLight {
+  vec3 direction;
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
 
 in vec2 uvs;
+in vec3 normals;
+in vec3 fragPos;
+
+out vec4 fragColor;
+
+uniform Material material;
+uniform DirectionalLight directionalLight;
+uniform vec3 viewPos;
+
+vec3 getDirLight(DirectionalLight light, vec3 normal, vec3 viewDir);
 
 void main() {
-    float ambientStr = 0.15;
-    vec3 ambient = ambientStr * lightColor;
-    vec3 result = color.rgb * lightColor;
+    vec3 norm = normalize(normals);
+    vec3 viewDir = normalize(viewPos - fragPos);
+
+    vec3 result = getDirLight(directionalLight, norm, viewDir);
     fragColor = vec4(result, 1.0);
+}
+
+vec3 getDirLight(DirectionalLight light, vec3 normal, vec3 viewDir) {
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0);
+
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+    vec3 ambient = light.ambient * texture(material.diffuse, uvs).rgb;
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, uvs).rgb; 
+    vec3 specular = light.specular * spec * texture(material.specular, uvs).rgb;
+
+    return (ambient + diffuse + specular);
 }
