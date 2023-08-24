fdc6b0157f0af7002a93d1876f2ec62f87876efd
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-08-25 02:56:43 +0300

    Implement obj loader

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -xe
 
-gcc -Wall -Wextra -o example_3d examples/example_3d.c renderer/renderer.c renderer/3d/renderer_3d.c glad.c io.c renderer/shader.c renderer/texture.c window.c renderer/3d/mesh.c renderer/camera.c renderer/3d/lighting.c renderer/material.c -lSDL2main -lSDL2 -lSDL2_image -lm
+gcc -Wall -Wextra -o example_3d examples/example_3d.c renderer/renderer.c renderer/3d/renderer_3d.c glad.c io.c renderer/shader.c renderer/texture.c window.c renderer/3d/mesh.c renderer/camera.c renderer/3d/lighting.c renderer/material.c obj_loader.c -lSDL2main -lSDL2 -lSDL2_image -lm
 
---
+++ obj_loader.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "renderer/3d/mesh.h"
+#include "io.h"
+
+Mesh *loadObj(const char *path);
